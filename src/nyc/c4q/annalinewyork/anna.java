3c44e8755a40b5c380ed63ea9970ba7f3ef70977
annalinewyork <annalinewyork@gmail.com>
2015-03-03 21:46:09 -0500

    push for hel

---
+++ src/nyc/c4q/annalinewyork/anna.java
@@ -0,0 +1,10 @@
+package nyc.c4q.annalinewyork;
+
+/**
+ * Created by c4q-nali on 3/2/15.
+ */
+public class anna {
+    public static void main (String [] args ) {
+        System.out.println("Hi,Anna");
+    }
+}
---
+++ variable/src/fristclass.java
@@ -0,0 +1,22 @@
+/**
+ * Created by c4q-nali on 3/3/15.
+ */
+public class fristclass {
+
+        public static void main( String[] args )
+        {
+            String holiday, myLove, myTree;
+
+            holiday = "Christmas";
+            myLove = " my true love sent to me ";
+            myTree = "a Partridge in a Pear Tree";
+
+            System.out.println( "On the First day of " + holiday +  myLove + "\n"+ myTree + "." );
+            System.out.println( "\n");
+            System.out.println( "On the Second day of " + holiday +  myLove + "\n" + "Two Turtle Doves and" + myTree + "." );
+            System.out.println( "\n");
+            System.out.println( "On the Third day of " + holiday +  myLove + "\n" + "Three French Hens, Two Turtle Doves and " + myTree + "." );
+
+
+        }
+    }
